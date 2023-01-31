f32fdc84171b971d469f0d71483a4c9d75ac6723
itsnotLonee <david.balboa.rojo@gmail.com>
2023-01-31 02:37:57 +0100

    fix for vite prod

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,6 +3,8 @@ import { URL, fileURLToPath } from 'node:url';
 import vue from '@vitejs/plugin-vue';
 import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify';
 import nodePolyfills from 'vite-plugin-node-stdlib-browser';
+import nodeStdlibBrowser from 'node-stdlib-browser';
+import inject from '@rollup/plugin-inject';
 
 export default defineConfig({
   plugins: [
@@ -15,12 +17,30 @@ export default defineConfig({
         configFile: 'src/styles/styles.scss',
       },
     }),
+    {
+      ...inject({
+        global: [
+          require.resolve('node-stdlib-browser/helpers/esbuild/shim'),
+          'global',
+        ],
+        process: [
+          require.resolve('node-stdlib-browser/helpers/esbuild/shim'),
+          'process',
+        ],
+        Buffer: [
+          require.resolve('node-stdlib-browser/helpers/esbuild/shim'),
+          'Buffer',
+        ],
+      }),
+      enforce: 'post',
+    },
     nodePolyfills(),
   ],
   define: { 'process.env': {} },
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      ...nodeStdlibBrowser,
     },
     extensions: [
       '.js',
@@ -31,6 +51,9 @@ export default defineConfig({
       '.tsx',
       '.vue',
     ],
+  },
+  optimizeDeps: {
+    include: [ 'buffer', 'process' ],
   },
   server: {
     port: 3000,
