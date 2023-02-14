63bda822859c22297eb5991123505eef695d0b07
ecstasy <david.balboa.rojo@gmail.com>
2023-02-14 20:58:39 +0100

    Added 3 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjE4NDM1OA/spectral.js?branch_slug=develop&token=1252c4e8-2137-4160-bab8-15dda7c4ce20";
+export default { extends: ruleset };
